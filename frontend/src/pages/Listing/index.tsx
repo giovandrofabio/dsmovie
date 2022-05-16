4789d92d33e1e7373c14e26a833280d99e1f37a4
giovandrofabio <giovandrofabiosantos@hotmail.com>
2022-05-15 22:44:00 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,16 +22,20 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size12&page=${pageNumber}&sort=id`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
             .then(responde => {
                 const data = responde.data as MoviePage;
                 setPage(data);
             })
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber : number) =>{
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
 
             <div className="container">
                 <div className="row">
